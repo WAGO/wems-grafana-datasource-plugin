b4b56d1aaa4432616577498697c54ff0ee21b84b
Martin-Meyer <martin.meyer@wago.com>
2025-08-14 10:40:10 +0200

    Update configEditor.spec.ts

--- tests/configEditor.spec.ts
+++ tests/configEditor.spec.ts
@@ -4,7 +4,7 @@ import { MyDataSourceOptions, MySecureJsonData } from '../src/types';
 test('smoke: should render config editor', async ({ createDataSourceConfigPage, readProvisionedDataSource, page }) => {
   const ds = await readProvisionedDataSource({ fileName: 'datasources.yml' });
   await createDataSourceConfigPage({ type: ds.type });
-  await expect(page.getByLabel('Path')).toBeVisible();
+  await expect(page.getByLabel('Client ID')).toBeVisible();
 });
 test('"Save & test" should be successful when configuration is valid', async ({
   createDataSourceConfigPage,
@@ -13,8 +13,8 @@ test('"Save & test" should be successful when configuration is valid', async ({
 }) => {
   const ds = await readProvisionedDataSource<MyDataSourceOptions, MySecureJsonData>({ fileName: 'datasources.yml' });
   const configPage = await createDataSourceConfigPage({ type: ds.type });
-  await page.getByRole('textbox', { name: 'Path' }).fill(ds.jsonData.path ?? '');
-  await page.getByRole('textbox', { name: 'API Key' }).fill(ds.secureJsonData?.apiKey ?? '');
+  await page.getByRole('textbox', { name: 'Client ID' }).fill(ds.jsonData.client_id ?? '');
+  await page.getByRole('textbox', { name: 'Client Secret' }).fill(ds.secureJsonData?.client_secret ?? '');
   await expect(configPage.saveAndTest()).toBeOK();
 });
 
@@ -25,7 +25,7 @@ test('"Save & test" should fail when configuration is invalid', async ({
 }) => {
   const ds = await readProvisionedDataSource<MyDataSourceOptions, MySecureJsonData>({ fileName: 'datasources.yml' });
   const configPage = await createDataSourceConfigPage({ type: ds.type });
-  await page.getByRole('textbox', { name: 'Path' }).fill(ds.jsonData.path ?? '');
+  await page.getByRole('textbox', { name: 'Client ID' }).fill(ds.jsonData.client_id ?? '');
   await expect(configPage.saveAndTest()).not.toBeOK();
-  await expect(configPage).toHaveAlert('error', { hasText: 'API key is missing' });
+  await expect(configPage).toHaveAlert('error', { hasText: 'Client secret is missing' });
 });
